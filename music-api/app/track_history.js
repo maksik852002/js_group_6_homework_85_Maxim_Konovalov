1e939898bfafd716420429b90a96b15da499cfaf
Maxim Konovalov <maksik852002@gmail.com>
2020-03-17 19:18:52 +0600

    add mongodb export

--- music-api/app/track_history.js
+++ music-api/app/track_history.js
@@ -9,19 +9,15 @@ router.get("/", auth, async (req, res) => {
   try {
     const album = await  Album.find().populate('artist');
     const history = await TrackHistory.find({user: req.user._id}).sort({datetime: -1}).limit(20).populate('track');
-    // console.log(history)
     for(alb of album) {
       for(hist of history) {
-        console.log(hist)
         if (hist.track.album.toString() === alb._id.toString()) {
           hist.artist = {_id: alb.artist._id, name: alb.artist.name }
         }
       }
      }
-    console.log(history)
     res.send(history);
   } catch (e) {
-    console.log(e)
     res.status(422).send(e);
   }	
 });
